b12ff5d9a348590fa76ebb19e1598327c1fb8e4a
mheymich <mheymich@hotmail.de>
2018-05-07 22:02:16 +0200

    Mein Teil zum Grundgeruest.

---
+++ submarine/src/main/java/com/AEProjekt/submarine/Submarine.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package submarine_addon;  // created at 5/7/2018
+
+/**
+ *
+ * @author Michel
+ */
+
+// renamed from UBoat to Submarine (Michel; 5/7/2018)
+public class Submarine {
+    private int xPosition;
+    private int yPosition;
+    
+    private void generatePosition(){
+        
+    }
+}
