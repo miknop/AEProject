fc4ba1950b5fe909131af96dabf4f6459062c2bb
tbackes <torben.backes@kisters.de>
2018-06-19 15:16:27 +0200

    Added Id to Entity.

--- submarine/src/main/java/com/AEProjekt/submarine/persistence/entities/UserEntity.java
+++ submarine/src/main/java/com/AEProjekt/submarine/persistence/entities/UserEntity.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 /**
  * Created by backes_tor on 14.06.2018.
@@ -15,7 +17,9 @@ import javax.persistence.Entity;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserEntity {
-
+    @Id
+    @GeneratedValue
+    private long id;
     private String username;
     private int level;
 }
